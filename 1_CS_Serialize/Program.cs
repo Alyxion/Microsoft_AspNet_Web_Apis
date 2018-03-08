758770bf8b735a1ffd1b4587a7c9f1fc0b8a0616
Michael Ikemann <ikemann@logiball.de>
2018-03-08 14:40:31 +0100

    Initial commit

---
+++ 1_CS_Serialize/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Create product object
+            var product1 = new Product { ID=101, Name="Red Apple", Price=1.99 };
+
+            // Serialize the objec to JSON
+            var jsonString = JsonConvert.SerializeObject(product1);
+
+            System.Console.WriteLine(jsonString);
+
+            // Deserialize
+            var product2 = JsonConvert.DeserializeObject<Product>(jsonString);
+
+            System.Console.WriteLine($"The name is "+product2.Name);
+            System.Console.WriteLine($"The ID is "+product2.ID);
+            System.Console.WriteLine($"The price is is "+product2.Price);
+        }
+    }
+
+    // Create model class
+    class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
